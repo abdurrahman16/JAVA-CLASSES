ff9b7a034d52735cb75776218d4d39501b70af0e
ラハマン <rahman.just16@gmail.com>
2026-02-21 12:34:20 +0900

    Encapsulation

--- Main.java
+++ Main.java
@@ -1,25 +1,10 @@
 public class Main {
-int ModelYear;
-String ModelName;
+    public static void main(String[] args){
+        Person obj = new Person();
+            
+           obj.setName("John");
+            System.out.println(obj.getName());
 
-public Main (String ModelName){
-    this(2020, ModelName);
 
-}
-public Main (int ModelYear, String ModelName){
-    this.ModelYear = ModelYear;
-    this.ModelName = ModelName; }
-
-public void CarInfo(){
-    System.out.println(ModelName+" And "+ ModelYear);
-    
-}
-
-public static void main(String[] args) {
-    Main car1= new Main("BMW");
-        car1.CarInfo();
-        Main car2 = new Main(2020, "Mercedes");
-        car2.CarInfo();
-  
-}
+    }
 }
